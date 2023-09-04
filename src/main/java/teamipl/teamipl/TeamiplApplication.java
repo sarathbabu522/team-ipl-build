44adeef01fcde3ac95186f24efbc42c838c3f2ff
root <root@ip-172-31-63-239.ec2.internal>
2023-09-04 13:47:22 +0000

    build ipl team

---
+++ src/main/java/teamipl/teamipl/TeamiplApplication.java
@@ -0,0 +1,13 @@
+package teamipl.teamipl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamiplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamiplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teamipl/teamipl/TeamiplApplicationTests.java
@@ -0,0 +1,13 @@
+package teamipl.teamipl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamiplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
